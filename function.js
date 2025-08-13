737002d93db93a0d5186b569664de6b35e43b7e8
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-13 19:45:06 +0600

    function intro

---
+++ function.js
@@ -0,0 +1,14 @@
+const number = 45;
+function turnOffTheFan() {
+  console.log("Stand up");
+  console.log("Walk toward the switch");
+  console.log("Click the switch to turn on the fan");
+}
+turnOffTheFan();
+
+function brushTeeth() {
+  console.log("pick up the brush");
+  console.log("add paste");
+  console.log("brush for two minutes");
+}
+brushTeeth();
---
+++ parameter.js
@@ -0,0 +1,30 @@
+function eatRice() {
+  console.log("wash your hands");
+  console.log("sit");
+  console.log("take the food in your plate");
+  console.log("eat");
+}
+// eatRice();
+
+function square(number) {
+  console.log("value of the number parameter", number);
+  const square = number * number;
+  console.log("square of the given number", square);
+}
+square(4);
+square(12);
+square(105);
+square(5);
+
+function add(num1, num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
+add(5, 71);
+add(65, 31);
+
+function addAll(a, b, c, d) {
+  const total = a + b + c + d;
+  console.log(total);
+}
+addAll(4, 5, 6, 7);
