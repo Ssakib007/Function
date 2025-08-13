4b79f05153448b62dfd1400b8aa6dd43bab5df17
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-13 21:10:18 +0600

    various kinds of parameters

---
+++ various_params.js
@@ -0,0 +1,39 @@
+/// for a given string, tell me whether it has even numbers of character or not
+
+function evenSizedString(str) {
+  const size = str.length;
+  console.log(str, size);
+  if (size % 2 === 0) {
+    console.log("even size");
+    return true;
+  } else {
+    console.log("odd size");
+    return false;
+  }
+}
+// evenSizedString("Dhaka");
+// evenSizedString("Ctg");
+
+function doubleOrTriple(number, doDouble) {
+  if (doDouble === true) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number * 3;
+    return result;
+  }
+}
+
+console.log(doubleOrTriple(5, true));
+console.log(doubleOrTriple(5, false));
+
+function numberOfElements(numbers) {
+  const len = numbers.length;
+  return len;
+}
+numberOfElements([12, 45, 78, 45, 121254, 4, 5]);
+
+function getAge(person){
+    const age = person.age
+    return age
+}
